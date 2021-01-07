ce5c51321881a2158080b6fcd86a34a30755c602
nriddiford <nickriddiford@me.com>
2021-01-07 09:27:50 +0100

    Add some R scripts

---
+++ plotting_cnv.R
@@ -0,0 +1,8 @@
+# Check where we are:
+getwd()
+
+# Set the directory to the 'cnvPlotteR' github repository
+setwd('')
+
+# load ('source') all of the code in 'allelefreqs.R':
+source('script/alleleFreqs.R')
