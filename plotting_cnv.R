5813e713858a5f2853befad727c3ae3ce91c5b11
nriddiford <nickriddiford@me.com>
2021-01-07 09:53:27 +0100

    Rename

--- plotting_cnv.R
+++
@@ -1,8 +0,0 @@
-# Check where we are:
-getwd()
-
-# Set the directory to the 'cnvPlotteR' github repository
-setwd('')
-
-# load ('source') all of the code in 'allelefreqs.R':
-source('script/alleleFreqs.R')
